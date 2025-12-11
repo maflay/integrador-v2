a941500cad4b7cc852985bd06908e78b964104a2
juanfelipe <maflay26@gmail.com>
2025-12-11 13:55:29 -0500

    cambios en url

--- dinamicas/user.js
+++ dinamicas/user.js
@@ -16,6 +16,13 @@ function getCookie(name) {
   return null;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  if (location.pathname.endsWith(".html")) {
+    const cleanPath = location.pathname.replace(/\.html$/, "");
+    history.replaceState(null, "", cleanPath);
+  }
+});
+
 function validateSession() {
   // // script de cada vista
   let objUser = getCookie(CS_KEY);
