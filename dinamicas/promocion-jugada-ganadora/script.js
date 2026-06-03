9294492fa3b0bf5b1a6f10e4ec1a18f5d3b8074a
juanfelipe <maflay26@gmail.com>
2026-06-03 11:18:20 -0500

    cambio jugada g

--- dinamicas/promocion-jugada-ganadora/script.js
+++ dinamicas/promocion-jugada-ganadora/script.js
@@ -33,7 +33,6 @@ const btn_send_secundario = document.getElementById("btn_send_secundario");
 const btn_envia_observacion = document.getElementById("btn_envia_observacion");
 const _board_jugada_ = document.getElementById("_board_jugada_");
 const btn_back_show_number = document.getElementById("btn_back_show_number");
-const _ficha_pierna_ = document.getElementById("_ficha_pierna_");
 
 const numeroAfuera = ["9", "10", "11", "12"];
 const numeroPalo = ["4", "17", "5", "6", "15", "16", "7", "8", "13", "14"];
@@ -81,7 +80,6 @@ function moveBallDina(posi, num) {
   } else {
     realNum = posi;
   }
-  _ficha_pierna_.classList.remove("pateada");
   setTimeout(() => {
     validateAlert(realNum);
   }, 800);
