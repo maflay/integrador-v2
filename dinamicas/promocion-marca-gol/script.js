c07d2d9e3f454eb07735d5c6a79acfea54ae5399
juanfelipe <maflay26@gmail.com>
2026-06-18 08:57:12 -0500

    cambio tabla fiebre

--- dinamicas/promocion-marca-gol/script.js
+++ dinamicas/promocion-marca-gol/script.js
@@ -557,7 +557,7 @@ function handleInfoGolazo() {
     timeZone: "America/Bogota",
     year: "numeric",
     month: "2-digit",
-    day: "2-digit",
+    day: "2-digit", 
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
