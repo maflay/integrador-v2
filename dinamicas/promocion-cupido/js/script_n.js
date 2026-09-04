acab7ce1f10b37c86c0f6ecc6295d199f44b7428
maflay <maflay26@gmail.com>
2026-09-04 09:20:07 -0500

    cambio de envio de resultado

--- dinamicas/promocion-cupido/js/script_n.js
+++ dinamicas/promocion-cupido/js/script_n.js
@@ -503,6 +503,13 @@ document.getElementById("start_game").addEventListener("click", () => {
 
 const btn_enviar = document.getElementById("btn_enviar");
 btn_enviar.addEventListener("click", () => {
+  if(categoria.value != "ADICIONAL" && matches == 0){
+    Swal.fire({
+      icon: "warning",
+      title: "Juego sin resultado"
+    });
+    return
+  }
   handleSubmit();
 });
 
